d68f7338c33e104b4f205140a09d88a90ddc5d17
Brian Schlining <bschlining@gmail.com>
2018-08-23 10:50:55 -0700

    Added typing

--- visualocean.py
+++ visualocean.py
@@ -1,13 +1,14 @@
 import requests
 import pandas as pd
+from typing import List
 
 
 class VisualOcean(object):
 
-    def __init__(self, base_url):
+    def __init__(self, base_url: str):
         self.base_url = base_url
 
-    def products(self):
+    def products(self) -> List[str]:
         url = "{}/parameters.json".format(self.base_url)
         json = requests.get(url).json()
         df = pd.DataFrame.from_records(json['data'])
